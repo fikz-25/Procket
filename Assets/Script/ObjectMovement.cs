1cc994615af7fdaebdf7e65810975b8bfce0b8f4
Fikri Ulinukha <ulinukhafikri@gmail.com>
2021-05-24 13:53:33 +0700

    procket game

---
+++ Assets/Script/ObjectMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
+    }
+}
